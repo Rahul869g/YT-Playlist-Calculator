5247629bfd3de4f0aa9fff0fc9ebdaae043d9b82
Rahul Gupta <rahul123alok@gmail.com>
2023-09-25 23:42:33 +0530

    added clear input with formik

--- playlist-calculator/src/components/Body.jsx
+++ playlist-calculator/src/components/Body.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useFormik } from "formik";
+import Playlist from "./Playlist";
 
 function Body() {
   const formik = useFormik({
@@ -12,31 +13,49 @@ function Body() {
     }
   });
 
+  // Function to clear the input field
+  const handleClearInput = () => {
+    formik.setFieldValue("playlistLink", ""); // Clear the input field
+  };
+
   return (
-    <div className="max-w-auto  mx-auto mt-10 p-4 bg-gray-800 rounded-lg shadow-md">
-      <h1 className="text-2xl md:text-3xl font-semibold text-white mb-4">
-        Find the length of any YouTube playlist:
-      </h1>
-      <form onSubmit={formik.handleSubmit}>
-        <div className="mb-4">
-          <input
-            type="text"
-            id="playlistLink"
-            name="playlistLink"
-            className="w-full px-3 py-2 bg-gray-700 text-white border rounded-lg focus:outline-none focus:border-blue-500"
-            placeholder="Enter YouTube Playlist Link"
-            onChange={formik.handleChange}
-            value={formik.values.playlistLink}
-          />
-        </div>
-        <button
-          type="submit"
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline-blue"
-        >
-          Analyze
-        </button>
-      </form>
-    </div>
+    <>
+      <div className="max-w-auto  mx-auto mt-10 p-4  rounded-lg ">
+        <h1 className="text-2xl md:text-3xl font-semibold text-white mb-4">
+          Find the length of any YouTube playlist:
+        </h1>
+        <form onSubmit={formik.handleSubmit}>
+          <div className="mb-4 relative">
+            <input
+              type="text"
+              id="playlistLink"
+              name="playlistLink"
+              className="w-full px-3 py-2 bg-gray-700 text-white border rounded-lg focus:outline-none focus:border-blue-500"
+              placeholder="Enter YouTube Playlist Link"
+              onChange={formik.handleChange}
+              value={formik.values.playlistLink}
+              autoFocus
+            />
+            {formik.values.playlistLink && (
+              <button
+                type="button"
+                className="absolute top-2 right-2 text-gray-500"
+                onClick={handleClearInput}
+              >
+                X
+              </button>
+            )}
+          </div>
+          <button
+            type="submit"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline-blue"
+          >
+            Analyze
+          </button>
+        </form>
+      </div>
+      <Playlist />
+    </>
   );
 }
 
