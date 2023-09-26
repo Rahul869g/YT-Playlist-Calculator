39a6c57aef89dbd85e0ac194332e644c8fb4a25c
Rahul Gupta <rahul123alok@gmail.com>
2023-09-26 23:42:21 +0530

    added Backend

---
+++ playlist-calculator/server/controller/linkController.js
@@ -0,0 +1,3 @@
+const { google } = require("googleapis");
+const youtube = google.youtube("v3");
+const api_key = process.env.API_KEY;
---
+++ playlist-calculator/server/index.js
@@ -0,0 +1,53 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+
+const path = require("path");
+const cors = require("cors");
+const corsOptions = require("./config/corsOptions");
+
+const link = require("./routes/api/link");
+
+const root = require("./routes/root");
+const PORT = process.env.PORT || 3000;
+
+//cross origin resource sharing
+app.use(cors(corsOptions));
+
+//built-in middleware to handle urlencoded data
+//in other words, form data:
+//"content-type: application/x-ww-form-urlencoded"
+app.use(express.urlencoded({ extended: false }));
+
+//built-in middleware to handle json data
+app.use(express.json());
+
+//middleware for cookies
+app.use(cookieParser());
+
+//serve static files
+app.use("/", express.static(path.join(__dirname, "/public")));
+
+// Make uploads folder public
+// app.use("/uploads", express.static("uploads"));
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+//routes
+app.use("/", root);
+
+app.use("/link", link);
+
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ error: "404 not Found" });
+  } else {
+    res.type("txt").send("404 not Found");
+  }
+});
+
+app.use(errorHandler);
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
