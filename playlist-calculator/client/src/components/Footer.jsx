74a4e70b8885611f963b5c0ba70ed9f761554eb2
Rahul Gupta <rahul123alok@gmail.com>
2023-09-29 23:09:49 +0530

    updated footer

--- playlist-calculator/client/src/components/Footer.jsx
+++ playlist-calculator/client/src/components/Footer.jsx
@@ -3,7 +3,17 @@ import React from "react";
 const Footer = () => {
   return (
     <>
-      <div className="bg-black fixed bottom-0 left-0 w-full p-8"></div>
+      <div className="bg-black fixed bottom-0 left-0 w-full p-5 md:p-8 ">
+        <p className="text-center text-white text-sm sm:text-base font-medium tracking-wide ">
+          Developed with{" "}
+          <button className="link heart-btn scale-100 hover:scale-125 transition-all">
+            <span role="img" aria-label="heart" className="animate-pulse">
+              ❤️
+            </span>
+          </button>{" "}
+          by <span className="text-white">Rahul Gupta</span>
+        </p>
+      </div>
     </>
   );
 };
